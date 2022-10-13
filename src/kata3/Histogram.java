d52efbd5e77c35e01d4f630da2143f6378efd87e
lagon <lagon@10.195.131.128>
2022-10-13 09:46:09 +0100

    Kata3 versión 2

--- src/kata3/Histogram.java
+++ src/kata3/Histogram.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package kata3;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-/**
- *
- * @author lagon
- */
 public class Histogram<T> {
     
     private final Map<T, Integer> map = new HashMap<T, Integer>();
