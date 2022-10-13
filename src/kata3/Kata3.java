71837ec3c972090db6df8cd094e44865ce114db8
lagon <lagon@10.195.131.128>
2022-10-13 09:21:51 +0100

    Kata3 versión 1

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,11 @@
+package kata3;
+
+public class Kata3 {
+
+    public static void main(String[] args) {
+        HistogramDisplay histogramDisplay = new HistogramDisplay("Histogram display");
+        histogramDisplay.execute();
+    }
+    
+}
+
